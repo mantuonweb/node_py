fda97d654b3d8018ead0147f2215aec0db0fc206
mantuonweb <mantu.nigam@globallogic.com>
2017-11-20 09:33:26 +0530

    added a new footer issuep[2]

--- machine.js
+++ machine.js
@@ -8,6 +8,7 @@ let regression = new SLR(x, y);
 regression.toString(3) === 'f(x) = - 0.265 * x + 50.6';
 //console.log(regression.predict(80));
 //console.log(regression.predict(30));
+//new changes
 PolynomialRegression = require('ml-regression').PolynomialRegression;
 x = [50,50,50,70,70,70,80,80,80,90,90,90,100,100,100];
 y = [3.3,2.8,2.9,2.3,2.6,2.1,2.5,2.9,2.4,3.0,0,2.8,3.3,3.5,4];
